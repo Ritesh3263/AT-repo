0b4f53bc10703f1cb553df98a0e822678175f0fd
Srikanth Mattupalli <smattupalli@sreebtech.com>
2023-12-01 07:51:56 +0530

    Updated owner profile information

--- src/app/pages/components/owner-profile/owner-profile.component.ts
+++ src/app/pages/components/owner-profile/owner-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-owner-profile',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./owner-profile.component.scss']
 })
 export class OwnerProfileComponent {
+  user: any = {}
+  constructor(private userService: UserService) {};
 
+  firstName = new FormControl('', [Validators.required]);
+  lastName = new FormControl('', [Validators.required]);
+  phoneNumber = new FormControl('');
+  ngOnInit() {
+    this.loadUserDetails();
+  }
+  loadUserDetails() {
+    this.userService.getUserDetails().then((user:any) => {
+      this.user = user || {};
+      console.log("jbjkbkb",this.user)
+      this.firstName.setValue(this.user.firstName)
+      this.lastName.setValue(this.user.lastName)
+      this.phoneNumber.setValue(this.user.phoneNumber)
+    })
+  }
 }
