66d0ad0e8cf512a7ecc2769bdc374f9cd0a62483
Srikanth Mattupalli <smattupalli@sreebtech.com>
2023-12-11 18:02:58 +0530

    Updated total values in popup

--- src/app/pages/components/my-basket-info/confirm-trade/confirm-trade.component.ts
+++ src/app/pages/components/my-basket-info/confirm-trade/confirm-trade.component.ts
@@ -1,7 +1,9 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { BasketTradeService } from 'src/app/services/basket-trade.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription, timer } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 export interface PeriodicElement {
   tickersymbol: string;
@@ -19,10 +21,25 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './confirm-trade.component.html',
   styleUrls: ['./confirm-trade.component.scss']
 })
-export class ConfirmTradeComponent {
+export class ConfirmTradeComponent implements OnInit {
   showSpinner:boolean=false;
+  investedAmount:any=null;
+  netInvestedAmount:any=null;
+  avalibleCashBalance: any=null;
+  originalData:any=[]
+  private subscription: any;
+
+
   constructor(public dialog: MatDialog,public dialogRef: MatDialogRef<ConfirmTradeComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,private basketTradeService:BasketTradeService,private matSnackBar:MatSnackBar) {
+      if(this.data && this.data.symbols){
+        this.data.symbols.forEach((element:any) => {
+          this.investedAmount = this.investedAmount+element.new_invested;
+          this.netInvestedAmount = this.netInvestedAmount+element.new_invested
+          this.avalibleCashBalance = this.data.cash_balance - this.investedAmount
+
+        });
+      }
   }
 
   displayedColumns: string[] = ['tickersymbol', 'shares', 'currentprice', 'investaccount'];
@@ -33,12 +50,12 @@ export class ConfirmTradeComponent {
 
 
   confirmOrder(){
-    
-
-    let input = {
+    if(this.data && this.data.symbols){
+    let input:{Type :string , Orders:object[]} = {"Type":"NORMAL",Orders:[]};
+    let object = {
       AccountID: "SIM1213784M",
-      Symbol: this.data.symbols[0].ticker_id,
-      Quantity: JSON.stringify(this.data.symbols[0].new_shares),
+      Symbol: "null",
+      Quantity: "null",
       OrderType: "Market",
       TradeAction: "BUY",
       TimeInForce: {
@@ -46,27 +63,78 @@ export class ConfirmTradeComponent {
         },
       Route: "Intelligent"
     }
-    console.log("this.data.symbols",input)
+ /**
+  * if symbols length is 1 the single order submit 
+  * else bulkOrder submits
+  */
+    if(this.data.symbols.length==1){
+      object.Symbol= this.data.symbols[0].ticker_id;
+      object.Quantity= JSON.stringify(this.data.symbols[0].new_shares);
+      this.showSpinner=true;
+      this.basketTradeService.setOrders(object,'nikhil','ts').then((data) => {
+        this.showSpinner =false;
+        if(data && data.Orders&& data.Orders[0].Error === 'FAILED'){
+          this.dialogRef.close();
+          this.matSnackBar.open(data.Orders[0].Message, 'Close', {
+            duration: 5000, // Duration in milliseconds
+            panelClass: ['custom-snack-bar'], 
+          });
+        }else{
+          this.matSnackBar.open(data.Orders[0].Message, 'Close', {
+            duration: 5000, // Duration in milliseconds
+            panelClass: ['custom-snack-bar'], 
+          });
+        }
+  
+      })
+    }else{
+    for(let i =0; i<this.data.symbols.length;i++){
+      object.Symbol= this.data.symbols[i].ticker_id;
+      object.Quantity= JSON.stringify(this.data.symbols[i].new_shares);
+      input.Orders.push(object)
+    }
     this.showSpinner=true;
-    this.basketTradeService.setOrders(input,'wences','ts').then((data) => {
+    this.basketTradeService.setBulkOrders(input,'nikhil','ts').then((data) => {
       this.showSpinner =false;
       if(data && data.Orders&& data.Orders[0].Error === 'FAILED'){
         this.dialogRef.close();
-        console.log("response",data.Orders,data.Orders[0])
         this.matSnackBar.open(data.Orders[0].Message, 'Close', {
           duration: 5000, // Duration in milliseconds
           panelClass: ['custom-snack-bar'], 
         });
         // this.toastrService.error('Marker closed, Order Failed', 'Error');
       }else{
-
+        this.matSnackBar.open(data.Orders[0].Message, 'Close', {
+          duration: 5000, // Duration in milliseconds
+          panelClass: ['custom-snack-bar'], 
+        });
       }
 
     })
   }
+  }
+  }
+  ngOnInit() {
+  //   this.originalData = this.data.symbols;
 
+  // this.subscription = timer(5000, 5000)
+  // .pipe(takeUntil(this.dialogRef.afterClosed()))
+  // .subscribe(() =>
+  // this.data.symbols[0].price =this.data.symbols[0].price + 5,
+  // this.data.symbols[3].price = this.data.symbols[3].price + 10);
+}
 
-
+//   getColor(price:any){
+//      let a =null
+// for(let i =0;i<this.originalData.length;i++){
+//   console.log(this.originalData[i].ticker_id,price.ticker_id ,this.originalData[i].price , price.price,"jdfvjsvksk")
+//   if(this.originalData[i].ticker_id == price.ticker_id && this.originalData[i].price != price.price){
+//     return "green"
+//   }
+// }
+    
+  //   return a;
+  // }
 
 
 }
