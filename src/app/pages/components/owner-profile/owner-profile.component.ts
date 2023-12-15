adbaf818757bc60c344d8f085b5e82a405404dca
Josh Pearson <jpearson-extendeed@outlook.com>
2023-12-15 10:54:38 -0700

    Angular Trading Backend - Owner Profile Page Completed

--- src/app/pages/components/owner-profile/owner-profile.component.ts
+++ src/app/pages/components/owner-profile/owner-profile.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { TraderService } from 'src/app/services/trader.service';
 import { BasketsService } from 'src/app/services/baskets.service';
 import { UtilitiesService } from 'src/app/services/utilities.service';
+import { UserService } from 'src/app/services/user.service';
 import { Basket } from 'src/app/interfaces/basket';
 
 @Component({
@@ -14,7 +15,8 @@ export class OwnerProfileComponent {
   trader: any = {};
   traderUserId!: string;
   baskets: Basket[] = [];
-  constructor(private traderService: TraderService, private utilityService: UtilitiesService, private basketService: BasketsService, private activatedRoute: ActivatedRoute) {
+  currentUser: any = {}
+  constructor(private traderService: TraderService, private utilityService: UtilitiesService, private basketService: BasketsService, private userService: UserService, private activatedRoute: ActivatedRoute) {
     this.activatedRoute.params.forEach((param) => {
       for(let key in param) {
         if(key == 'id') {
@@ -29,6 +31,9 @@ export class OwnerProfileComponent {
   }
 
   loadTraders() {
+    this.userService.getUserDetails().then((user:any) => {
+      this.currentUser = user || {};
+    })
     this.traderService.getPublisherProfile(this.traderUserId).then((trader:any) => {
       if(trader && trader.trader && trader.success) {
         this.trader = trader.trader;
@@ -57,9 +62,12 @@ export class OwnerProfileComponent {
   }
 
   follow() {
-    this.traderService.follow(this.trader.id, 'PUT').then((results:any) => {
+    this.trader.is_followed = !this.trader.is_followed;
+    this.trader.followers += this.trader.is_followed ? 1 : -1;
+    this.traderService.follow(this.trader.id, this.trader.is_followed ? 'PUT' : 'DELETE').then((results:any) => {
       if(results && results.success) {
-        this.utilityService.displayInfoMessage("Trader followed!  You will receive notifications when this trader publishes any changes")
+        this.utilityService.displayInfoMessage(this.trader.is_followed ? "Trader followed!  You will receive notifications when this trader publishes any changes." :
+          "Trader unfollowed!  You will no longer receive notifications when this trader publishes any changes.")
       }
       else {
         this.utilityService.displayInfoMessage(JSON.stringify(results), true);
@@ -79,10 +87,6 @@ export class OwnerProfileComponent {
       }
       else {
         this.utilityService.displayInfoMessage(basket.is_favorite ? "Basket added to favorites" : "Basket removed from favorites");
-        if(!basket.is_favorite) {
-          // Remove this basket from in-memory array
-
-        }
       }
     })
   }
@@ -96,10 +100,6 @@ export class OwnerProfileComponent {
       }
       else {
         this.utilityService.displayInfoMessage(basket.is_subscribed ? "Subscribed to basket." : "Unsubscribed from basket.")
-        if(!basket.is_subscribed) {
-          // Remove this basket from in-memory array
-
-        }
       }
     })
   }
