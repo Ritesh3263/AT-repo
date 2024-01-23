c8d29dfde2bc719792f01fb4b11dd1ccd51dbd37
Josh Pearson <jpearson-extendeed@outlook.com>
2024-01-23 11:46:38 -0700

    DevOps-177 - encodeUriComponent on search text

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -61,7 +61,7 @@ export class AdminService {
 
   async getNotifications(pageNumber: number, pageSize: number, sortColumn: string | null = null, sortMode: string | null = null, search: string | null = null) {
     try{
-      let res = await fetch(`${environment.apiBaseUrl}/admin-api/notifications?pageNumber=${pageNumber}&pageSize=${pageSize}${sortColumn ? `&sortColumn=${sortColumn}` : ''}${sortMode ? `&sortMode=${sortMode}` : ''}${search ? `&search=${search}` : ''}`, this.getHeaders());
+      let res = await fetch(`${environment.apiBaseUrl}/admin-api/notifications?pageNumber=${pageNumber}&pageSize=${pageSize}${sortColumn ? `&sortColumn=${sortColumn}` : ''}${sortMode ? `&sortMode=${sortMode}` : ''}${search ? `&search=${encodeURIComponent(search)}` : ''}`, this.getHeaders());
       let data = await res.json();
       return data;
     }
@@ -72,7 +72,7 @@ export class AdminService {
 
   async getUsers(pageNumber: number, pageSize: number, sortColumn: string | null = null, sortMode: string | null = null, search: string | null = null) {
     try{
-      let res = await fetch(`${environment.apiBaseUrl}/admin-api/users?pageNumber=${pageNumber}&pageSize=${pageSize}${sortColumn ? `&sortColumn=${sortColumn}` : ''}${sortMode ? `&sortMode=${sortMode}` : ''}${search ? `&search=${search}` : ''}`, this.getHeaders());
+      let res = await fetch(`${environment.apiBaseUrl}/admin-api/users?pageNumber=${pageNumber}&pageSize=${pageSize}${sortColumn ? `&sortColumn=${sortColumn}` : ''}${sortMode ? `&sortMode=${sortMode}` : ''}${search ? `&search=${encodeURIComponent(search)}` : ''}`, this.getHeaders());
       let data = await res.json();
       return data;
     }
